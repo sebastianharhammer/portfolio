b1060681903a58ed547f783bc95f54cd48c3f342
Na3r0 <96481684+sebastianharhammer@users.noreply.github.com>
2025-02-26 11:55:33 +0100

    Simplify header navigation hover effect with CSS clip-path

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -4,30 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  isHovered = false;
-  aboutMe = false;
-  skills = false;
-  projects = false;
-  thoughts = false;
-  contact = false;
-  
-  
-  onMouseOver(element: any) {
-    this.isHovered = true;
-    element = true;
-    console.log('mouse over' + element);
-  }
-  
-  onMouseLeave(element: any) {
-    this.isHovered = false;
-    this.aboutMe = false;
-    this.skills = false;
-    this.projects = false;
-    this.thoughts = false;
-    this.contact = false;
-    console.log('mouse leave' + element);
-  }
+  // No hover-related code needed!
 }
