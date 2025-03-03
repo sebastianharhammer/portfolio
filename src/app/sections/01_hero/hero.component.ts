c6549df15871bb1dce36af94399b30201f4b8dc2
Sebastian Harhammer <sebastian.harhammer@gmail.com>
2025-03-03 21:35:25 +0100

    Enhance Hero section with letter-by-letter animation and interactive hover effects

--- src/app/sections/01_hero/hero.component.ts
+++ src/app/sections/01_hero/hero.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
-
+frontend = ['r', 'o', 'n', 't', 'e', 'n', 'd'];
+developer = ['D', 'E', 'V', 'E', 'L', 'O', 'P', 'E', 'R'];
 }
