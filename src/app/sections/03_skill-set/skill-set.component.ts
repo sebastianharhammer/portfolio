e432bf5494c52b23264b84fd63a138f13a0df496
Na3r0 <96481684+sebastianharhammer@users.noreply.github.com>
2025-03-04 16:56:00 +0100

    Enhance Skill Set section sticker interaction with multi-stage peel-off animation and additional text/icon layers

--- src/app/sections/03_skill-set/skill-set.component.ts
+++ src/app/sections/03_skill-set/skill-set.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-skill-set',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './skill-set.component.html',
-  styleUrl: './skill-set.component.scss'
+  styleUrl: './skill-set.component.scss',
 })
 export class SkillSetComponent {
-  img = { src: 'assets/img/skills/sticker.svg', alt: 'sticker', title: 'Sticker' };
+ 
+
+  img = {
+    src: 'assets/img/skills/sticker.svg',
+    alt: 'sticker',
+    title: 'Sticker',
+  };
+
   icons = [
     { src: 'assets/img/skills/html.svg', alt: 'HTML', title: 'HTML' },
     { src: 'assets/img/skills/css.svg', alt: 'CSS', title: 'CSS' },
@@ -17,24 +26,54 @@ export class SkillSetComponent {
     { src: 'assets/img/skills/angular.svg', alt: 'Angular', title: 'Angular' },
     { src: 'assets/img/skills/restApi.svg', alt: 'restApi', title: 'Rest API' },
     { src: 'assets/img/skills/git.svg', alt: 'git', title: 'Git' },
-    { src: 'assets/img/skills/firebase.svg', alt: 'firebase', title: 'Firebase' },
+    {
+      src: 'assets/img/skills/firebase.svg',
+      alt: 'firebase',
+      title: 'Firebase',
+    },
     { src: 'assets/img/skills/scrum.svg', alt: 'scrum', title: 'Scrum' },
-    { src: 'assets/img/skills/materialDesign.svg', alt: 'materialDesign', title: 'Material Design' }
+    {
+      src: 'assets/img/skills/materialDesign.svg',
+      alt: 'materialDesign',
+      title: 'Material Design',
+    },
   ];
-  peelOffImages = [
-    { src: 'assets/img/skills/sticker.svg', alt: 'sticker' },
-    { src: 'assets/img/skills/sticker_first_peel.png', alt: 'sticker_first_peel'},
-    { src: 'assets/img/skills/sticker_peel_off.png', alt: 'sticker_peel_off' }
-  ];
+  
+    peelOffImages = [
+      {
+        src: 'assets/img/skills/sticker_full.png',
+        alt: 'sticker',
+        topPart: 'assets/img/skills/sticker_arrow.svg',
+        bottomPart: 'assets/img/skills/sticker_text_closed_en.svg',
+      },
+      {
+        src: 'assets/img/skills/sticker_first_peel.png',
+        alt: 'sticker_first_peel',
+        topPart: '',
+        bottomPart: '',
+      },
+      {
+        src: 'assets/img/skills/sticker_peel_off.png',
+        alt: 'sticker_peel_off',
+        topPart: 'assets/img/skills/sticker_text_open_en.svg',
+        bottomPart: 'assets/img/skills/sticker_icons_open.svg',
+      },
+    ];
 
-  peelOffIndex = 0; // Track which image is currently shown
-  currentImage = this.peelOffImages[this.peelOffIndex]; // Initialize with the first image
+
+  peelOffIndex = 0;
+  currentImage = this.peelOffImages[this.peelOffIndex];
 
   peelOff() {
     if (this.peelOffIndex < this.peelOffImages.length - 1) {
-      this.peelOffIndex++;
-      this.currentImage = this.peelOffImages[this.peelOffIndex];
+      setTimeout(() => {
+        this.peelOffIndex++;
+        this.currentImage = this.peelOffImages[this.peelOffIndex];
+      }, 500);
+      setTimeout(() => {
+        this.peelOffIndex++;
+        this.currentImage = this.peelOffImages[this.peelOffIndex];
+      }, 250);
     }
   }
 }
-
