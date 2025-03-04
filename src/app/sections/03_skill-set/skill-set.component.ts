af6df6bc17af0ff7565cd82cb8c609166eeddd53
Na3r0 <96481684+sebastianharhammer@users.noreply.github.com>
2025-03-04 10:23:05 +0100

    Implement interactive sticker peel-off animation in Skill Set section

--- src/app/sections/03_skill-set/skill-set.component.ts
+++ src/app/sections/03_skill-set/skill-set.component.ts
@@ -8,6 +8,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './skill-set.component.scss'
 })
 export class SkillSetComponent {
+  img = { src: 'assets/img/skills/sticker.svg', alt: 'sticker', title: 'Sticker' };
   icons = [
     { src: 'assets/img/skills/html.svg', alt: 'HTML', title: 'HTML' },
     { src: 'assets/img/skills/css.svg', alt: 'CSS', title: 'CSS' },
@@ -20,4 +21,20 @@ export class SkillSetComponent {
     { src: 'assets/img/skills/scrum.svg', alt: 'scrum', title: 'Scrum' },
     { src: 'assets/img/skills/materialDesign.svg', alt: 'materialDesign', title: 'Material Design' }
   ];
+  peelOffImages = [
+    { src: 'assets/img/skills/sticker.svg', alt: 'sticker' },
+    { src: 'assets/img/skills/sticker_first_peel.png', alt: 'sticker_first_peel'},
+    { src: 'assets/img/skills/sticker_peel_off.png', alt: 'sticker_peel_off' }
+  ];
+
+  peelOffIndex = 0; // Track which image is currently shown
+  currentImage = this.peelOffImages[this.peelOffIndex]; // Initialize with the first image
+
+  peelOff() {
+    if (this.peelOffIndex < this.peelOffImages.length - 1) {
+      this.peelOffIndex++;
+      this.currentImage = this.peelOffImages[this.peelOffIndex];
+    }
+  }
 }
+
