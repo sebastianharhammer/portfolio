a1a4448ed8bc7ffb360c81e13c379645c82ddfbb
Na3r0 <96481684+sebastianharhammer@users.noreply.github.com>
2025-03-05 13:37:23 +0100

    Implement language toggle functionality with ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { HeaderComponent } from "./shared/header/header.component";
 import { HeroComponent } from "./sections/01_hero/hero.component";
 import { AboutMeComponent } from "./sections/02_about-me/about-me.component";
@@ -10,10 +11,16 @@ import { ContactComponent } from "./sections/06_contact/contact.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, AboutMeComponent, SkillSetComponent, ProjectsComponent, ThoughtsComponent, ContactComponent, FooterComponent],
+  standalone: true,
+  imports: [TranslateModule, RouterOutlet, HeaderComponent, HeroComponent, AboutMeComponent, SkillSetComponent, ProjectsComponent, ThoughtsComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'PORTFOLIO';
+  /* constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  } */
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
+import translationEN from '../../../../public/assets/i18n/en.json';
+import translationDE from '../../../../public/assets/i18n/de.json';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
+  standalone: true,
+  providers: [TranslateService],
 })
 export class HeaderComponent {
+  /* constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  } */
+  
+  constructor(private translate: TranslateService) {
+    translate.setTranslation('en', translationEN);
+    translate.setTranslation('de', translationDE);
+    translate.setDefaultLang('en');
+  }
+  useLanguage(language: string) {
+    this.translate.use(language);
+    
+  }
   onMouseEnter() {
     console.log('mouse enter');
   }
