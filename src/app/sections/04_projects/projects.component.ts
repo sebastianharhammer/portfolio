b87b1b0ff532ed0bfbdeed78f1d43ed3d2d8883f
Sebastian Harhammer <sebastian.harhammer@gmail.com>
2025-03-06 15:55:05 +0100

    Enhance Projects section with dynamic project detail modal and image updates

--- src/app/sections/04_projects/projects.component.ts
+++ src/app/sections/04_projects/projects.component.ts
@@ -1,13 +1,27 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { LanguageService } from '../../services/language.service/language.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
 
 @Component({
   selector: 'app-projects',
-  imports: [TranslateModule],
+  imports: [TranslateModule, ProjectDetailComponent, CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
  languageService = inject(LanguageService);
+ selectedProject: string = '';
+
+ isProjectDetailVisible = false;
+
+ openProjectDetail(project: string) {
+  this.isProjectDetailVisible = true;
+  this.selectedProject = project;
+ }
+
+ closeProjectDetail() {
+  this.isProjectDetailVisible = false;
+ }
 }
