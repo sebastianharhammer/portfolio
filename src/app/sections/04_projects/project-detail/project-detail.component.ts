e0db6a1ece3488a6a0ce0f5b99eb32913f8dcd26
Na3r0 <96481684+sebastianharhammer@users.noreply.github.com>
2025-03-10 15:45:55 +0100

    Update project detail component with internationalization and styling improvements

--- src/app/sections/04_projects/project-detail/project-detail.component.ts
+++ src/app/sections/04_projects/project-detail/project-detail.component.ts
@@ -1,13 +1,11 @@
 import { ProjectsComponent } from '../projects.component';
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter,   } from '@angular/core';
 import { HeaderComponent } from '../../../shared/header/header.component';
-import { CommonModule, NgClass } from '@angular/common';
+import { CommonModule, } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { LogoComponent } from '../../../shared/logo/logo.component';
 
 interface ProjectDetails {
-  title: string;
-  description: string;
   technologies: string[];
   images: string[];
   github?: string;
@@ -16,7 +14,7 @@ interface ProjectDetails {
 
 @Component({
   selector: 'app-project-detail',
-  imports: [HeaderComponent, CommonModule, TranslateModule, LogoComponent, NgClass],
+  imports: [HeaderComponent, CommonModule, TranslateModule, LogoComponent],
   templateUrl: './project-detail.component.html',
   styleUrl: './project-detail.component.scss',
   standalone: true,
@@ -26,25 +24,19 @@ export class ProjectDetailComponent {
   @Output() close = new EventEmitter<void>();
 
   projectDetails: Record<string, ProjectDetails> = {
-    polloloco: {
-      title: 'El Pollo Loco',
-      description: 'A jump and run game built with JavaScript',
+    ellpolloloco: {
       technologies: ['JavaScript', 'HTML', 'CSS'],
       images: ['assets/img/projects/polloloco.jpg'],
       github: 'https://github.com/yourusername/el-pollo-loco',
       live: 'https://your-demo-url.com',
     },
     join: {
-      title: 'join',
-      description: 'A social media platform built with Angular and Firebase',
       technologies: ['Angular', 'Firebase', 'TypeScript'],
       images: ['assets/img/projects/join.jpg'],
       github: 'https://github.com/yourusername/join',
       live: 'https://your-demo-url.com',
     },
     pokedex: {
-      title: 'Pokedex',
-      description: 'A Pokedex built with Angular and PokeAPI',
       technologies: ['Angular', 'PokeAPI', 'TypeScript'],
       images: ['assets/img/projects/pokedex.jpg'],
       github: 'https://github.com/yourusername/pokedex',
