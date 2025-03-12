11f93e6f893b1f59accd8aad7b2e6ad9b437e4d4
Na3r0 <96481684+sebastianharhammer@users.noreply.github.com>
2025-03-12 16:52:52 +0100

    Enhance contact form user experience with improved error messages and validation feedback

---
+++ src/app/shared/picture-frame/picture-frame.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-picture-frame',
+  imports: [],
+  templateUrl: './picture-frame.component.html',
+  styleUrl: './picture-frame.component.scss'
+})
+export class PictureFrameComponent {
+
+}
