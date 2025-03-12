60b3eaed3ff70fbb1a642c09691518608ec2fb2a
Na3r0 <96481684+sebastianharhammer@users.noreply.github.com>
2025-03-12 11:45:03 +0100

    Implement form validation and data binding for contact section

--- src/app/sections/06_contact/contact.component.ts
+++ src/app/sections/06_contact/contact.component.ts
@@ -2,13 +2,26 @@ import { Component, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { LanguageService } from '../../services/language.service/language.service';
 import { LogoComponent } from '../../shared/logo/logo.component';
-
+import { FormsModule, NgForm } from '@angular/forms';
 @Component({
   selector: 'app-contact',
-  imports: [TranslateModule, LogoComponent],
+  imports: [TranslateModule, LogoComponent, FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
+  standalone: true
 })
 export class ContactComponent {
   languageService = inject(LanguageService);
+
+  contactData = {
+    name: '',
+    email: '',
+    message: ''
+  }
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+    }
+  }
 }
