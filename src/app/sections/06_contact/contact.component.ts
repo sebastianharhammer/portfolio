d1a58303593af64f4121ef0ffba3e3b182273e83
Sebastian Harhammer <sebastian.harhammer@gmail.com>
2025-03-12 14:17:14 +0100

    Add form validation and email submission logic to contact component

--- src/app/sections/06_contact/contact.component.ts
+++ src/app/sections/06_contact/contact.component.ts
@@ -3,6 +3,7 @@ import { TranslateModule } from '@ngx-translate/core';
 import { LanguageService } from '../../services/language.service/language.service';
 import { LogoComponent } from '../../shared/logo/logo.component';
 import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-contact',
   imports: [TranslateModule, LogoComponent, FormsModule],
@@ -12,6 +13,7 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class ContactComponent {
   languageService = inject(LanguageService);
+  http = inject(HttpClient);
 
   contactData = {
     name: '',
@@ -19,9 +21,35 @@ export class ContactComponent {
     message: ''
   }
 
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
   onSubmit(ngForm: NgForm) {
-    if (ngForm.valid && ngForm.submitted) {
-      console.log(this.contactData);
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            //HIER KOMMT DIE RESPONSE OVERLAY FUNKTION
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
     }
   }
 }
