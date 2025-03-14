4fabdff03542a9836cfa67ad2ac30883a973274e
Sebastian Harhammer <sebastian.harhammer@gmail.com>
2025-03-14 19:23:42 +0100

    Add privacy policy content and legal notice component
    - Introduced a comprehensive privacy policy section in the English localization file (en.json) with detailed information on data collection, rights, and legal notices.
    - Developed the legal notice component with structured HTML to display privacy policy content, including sections for overview, responsible entity details, and user rights.
    - Implemented styles for the legal notice component to enhance layout and user experience.
    - Updated the component logic to include navigation functionality for returning to the previous page.

--- src/app/pages/legal-notice/legal-notice.component.ts
+++ src/app/pages/legal-notice/legal-notice.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageService } from '../../services/language.service/language.service';
+import { CommonModule } from '@angular/common';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
+  languageService = inject(LanguageService);
+  constructor(private location: Location) {}
 
+  goBack(): void {
+    this.location.back();
+  }
 }
