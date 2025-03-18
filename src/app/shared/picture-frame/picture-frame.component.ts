e9ec1b5bcb50ea93171af5478f4cda10c59a49c8
Sebastian Harhammer <sebastian.harhammer@gmail.com>
2025-03-18 18:04:36 +0100

    Update localization, enhance project detail layout, and improve picture frame component
    - Added new localization entries for the "me" text in both German and English.
    - Updated the favicon reference in the HTML file for consistency.
    - Refactored the hero component to use a custom picture frame component for better structure.
    - Enhanced the project detail component with a new background element for improved visual layering.
    - Adjusted styles in the picture frame component for better responsiveness and hover effects.

--- src/app/shared/picture-frame/picture-frame.component.ts
+++ src/app/shared/picture-frame/picture-frame.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageService } from '../../services/language.service/language.service';
 @Component({
   selector: 'app-picture-frame',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './picture-frame.component.html',
   styleUrl: './picture-frame.component.scss'
 })
 export class PictureFrameComponent {
-
+  languageService = inject(LanguageService);
+  
 }
