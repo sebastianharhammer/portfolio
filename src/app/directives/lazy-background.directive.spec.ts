807fdb42833ccc97eb0ea48c429ee46a3e0ad35c
Harhammer Sebastian <sharhammer@sysob.com>
2026-02-25 09:17:07 +0100

    updated to 20.x

--- src/app/directives/lazy-background.directive.spec.ts
+++ src/app/directives/lazy-background.directive.spec.ts
@@ -1,4 +1,35 @@
+import { ElementRef, Renderer2 } from '@angular/core';
 import { LazyBackgroundDirective } from './lazy-background.directive';
+
+describe('LazyBackgroundDirective', () => {
+  let directive: LazyBackgroundDirective;
+
+  beforeEach(() => {
+    const elementRefMock = {
+      nativeElement: document.createElement('div')
+    } as ElementRef;
+
+    const rendererMock = jasmine.createSpyObj('Renderer2', [
+      'setStyle',
+      'addClass',
+      'removeClass'
+    ]);
+
+    directive = new LazyBackgroundDirective(
+      elementRefMock,
+      rendererMock
+    );
+  });
+
+  it('should create an instance', () => {
+    expect(directive).toBeTruthy();
+  });
+});
+
+
+
+
+/* import { LazyBackgroundDirective } from './lazy-background.directive';
 
 describe('LazyBackgroundDirective', () => {
   it('should create an instance', () => {
@@ -6,3 +37,4 @@ describe('LazyBackgroundDirective', () => {
     expect(directive).toBeTruthy();
   });
 });
+ */
--- src/app/services/language.service/language.service.spec.ts
+++ src/app/services/language.service/language.service.spec.ts
@@ -1,4 +1,41 @@
 import { TestBed } from '@angular/core/testing';
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from './language.service';
+
+describe('LanguageService', () => {
+  let service: LanguageService;
+
+  const translateMock = {
+    setTranslation: jasmine.createSpy('setTranslation'),
+    setDefaultLang: jasmine.createSpy('setDefaultLang'),
+    use: jasmine.createSpy('use'),
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: TranslateService, useValue: translateMock },
+      ],
+    });
+
+    service = TestBed.inject(LanguageService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should initialize translations and set default language', () => {
+    expect(translateMock.setTranslation).toHaveBeenCalledWith('en', jasmine.any(Object));
+    expect(translateMock.setTranslation).toHaveBeenCalledWith('de', jasmine.any(Object));
+    expect(translateMock.setDefaultLang).toHaveBeenCalledWith('en');
+    expect(translateMock.use).toHaveBeenCalled(); // called with active language
+  });
+});
+
+
+
+/* import { TestBed } from '@angular/core/testing';
 
 import { LanguageService } from './language.service';
 
@@ -14,3 +51,4 @@ describe('LanguageService', () => {
     expect(service).toBeTruthy();
   });
 });
+ */
